6f361657e345dca945a521a6d704d41c7bda1c0e
dagimalemux <dagimalemux@gmail.com>
2024-01-04 16:10:47 +0300

    not important

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
-
-// class based component with arrow functions
 import React from 'react'
-
-export const App = () => {
-  return (
-    <div>App</div>
-  )
-}
-// function based component 
-import React from 'react'
+import "./style.css"
 
 export default function App() {
   return (
     <div>App</div>
   )
 }
-// class based component 
-import React, { Component } from 'react'
-
-export default class App extends Component {
-  render() {
-    return (
-      <div>App</div>
-    )
-  }
-}
-
