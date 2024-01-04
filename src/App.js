f4cbb19656d5ab73a6ae36d25f46c5c85e6155d2
dagimalemux <dagimalemux@gmail.com>
2024-01-04 15:59:07 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      
     </div>
   );
 }
