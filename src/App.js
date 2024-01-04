091e5f424b38b2a129f938e7c22eb151e88cd4f1
dagimalemux <dagimalemux@gmail.com>
2024-01-04 16:06:38 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import './style.css';
+
+// class based component with arrow functions
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+// function based component 
+import React from 'react'
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
-  );
+    <div>App</div>
+  )
 }
+// class based component 
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
+}
+
