332a08ec20520b2b5801b9bf73dba226bbc79ab5
dagimalemux <dagimalemux@gmail.com>
2024-01-04 17:37:25 +0300

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Logo from './assets/logo.png'
 export const Header = () => {
   return (
     <div>
-         
+         <img {...Logo} alt="" />
       
     </div>
   )
